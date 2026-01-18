453c03df117314053be9d6967e752f19c6570aab
Fernando Jerez <fernando.jerez@fernandos-MacBook-Pro.local>
2026-01-18 23:41:54 +0100

    linter fixes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -162,10 +162,10 @@ def get_dealer_reviews(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except BaseException:
             return JsonResponse(
